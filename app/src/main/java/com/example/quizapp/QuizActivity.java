34e619554197fc7d29d674ff8a7fcb6c9ea9d405
Sebastian <s.oskiewicz@gmail.com>
2020-02-21 18:01:45 +0100

    Create SQLite database & start implementing

--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -3,12 +3,34 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private TextView textViewQuestion;
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
+    private TextView textViewCountDown;
+    private RadioGroup rbGroup;
+    private RadioButton rb1, rb2, rb3, rb4;
+    private Button buttonConfirm;
+
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        textViewQuestion = findViewById(R.id.textQuestionContent);
+        textViewScore = findViewById(R.id.textScore);
+        textViewQuestionCount = findViewById(R.id.textQuestionsCount);
+        textViewCountDown = findViewById(R.id.textTimeLeft);
+        rbGroup = findViewById(R.id.radioGroup);
     }
 }
